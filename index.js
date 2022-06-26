48a5008060e99682762c927b1606b6048a5da393
Bedra Kraja <bedra.kraja22@gmail.com>
2022-06-27 00:08:58 +0200

    Yep

--- index.js
+++ index.js
@@ -10,10 +10,47 @@ let state={
         },
         {
             text: 'Work out',
+            completed:false
+        },
+        {
+            text: 'Read a book',
             completed:true
         }
     ],
     showcompleted:false,
+}
+
+function createnewtodo(text){
+    let isinthelist= state.todos.some(todo=>todo.text===text)
+    if(isinthelist)
+    state.todos.push({text: text, completed: false})
+}
+
+function getincompletetodos() {
+    return state.todos.filter(todo => todo.completed === false)
+}
+   
+function getcompletetodos() {
+    return state.todos.filter(todo => todo.completed === true)
+}
+   
+function deleteodo(text) {
+    let thetodos = state.todos.filter(todo => todo.text !== text)
+    state.todos = thetodos
+}
+ 
+function deletealltodos() {
+    state.todos = []
+}
+   
+function toggletodo(text) {
+    let match = state.todos.find(todo => todo.text === text)
+    if (!match) return
+    match.completed = !match.completed
+}
+ 
+function toggleshowcompleted() {
+    state.showCompleted = !state.showCompleted
 }
 
 function renderoptionsection(){
@@ -26,39 +63,56 @@ function renderoptionsection(){
     
     let showcompletelabel=document.createElement('label')
     
+    
+    
     let showcompleteinput=document.createElement('input')
     showcompleteinput.type= 'checkbox'
-    showcompleteinput.addEventListener('click', function () {
-        for (let todo of state.todos) {
-          todo.done = true
-        }
-        renderoptionsection()
-      })
+    
+    if(state.showcompleted) showcompleteinput.checked=true
+    showcompleteinput.addEventListener("click",function(){
+        toggleshowcompleted()
+        render()
+    })
 
-    showcompletelabel.append(' Show completed', showcompleteinput)
+    showcompletelabel.append( " Show completed   ",showcompleteinput)
     optionSection.append(optionTitle, showcompletelabel)
-
 
     let appEl=document.querySelector('.app')
     appEl.append(optionSection)
 }
-function renderapptodo(){
-   let formEl = document.querySelector('form')
+function renderapptodo(){   
+ 
+    let additem= document.createElement('section')
 
-   let titleEl = document.createElement('h2')
-        titleEl.className='section_title'
-        titleEl.innerText= 'Add todo'
+    let additemtittle=document.createElement('h2')
+    additemtittle.classList.add("section_title")
+    additemtittle.textContent="Add Item"
 
-    let addinputEl = document.createElement('Input')
-    addinputEl.setAttribute('type', 'text')
-    addinputEl.setAttribute('name', 'text')
+    let newitem= document.createElement('form')
+    newitem.classList.add("Regullo")
 
-    let addbuttonEl= document.createElement('button')
-    addbuttonEl.className='todo_add'
-    addbuttonEl.innerText= 'add'
+    let newiteminput = document.createElement('input')
+    newiteminput.type="text"
+    newiteminput.placeholder="Add new item"
 
-    
+    let newitembutton = document.createElement('button')
+    newitembutton.type='submit'
+    newitembutton.textContent="+"
+
+    newitem.addEventListener('submit', function(event){
+        event.preventDefault()
+        createnewtodo(newiteminput.value)
+        render()
+    })
+
+    newitem.append(newiteminput, newitembutton)
+    additem.append(additemtittle,newitem)
+
+    let appEl=document.querySelector('.app')
+    appEl?.append(additem)
+
 }
+
 function rendertodosection(){
         let todosection = document.createElement('section')
 
@@ -76,7 +130,11 @@ function rendertodosection(){
 
         let todoitemEl= document.createElement('li')
         todoitemEl.className= 'todo'
-
+        
+        todoitemEl.addEventListener('click', function(){
+            toggletodo(todo.text)
+            render()
+        })
         let checkboxEl = document.createElement('Input')
         checkboxEl.type= 'checkbox'
         checkboxEl.className= 'todo_checkbox'
@@ -84,20 +142,16 @@ function rendertodosection(){
         let todotextEl= document.createElement("p")
         todotextEl.className= 'todo_text'
         todotextEl.innerText= todo.text
-        todotextEl.addEventListener('click', function () {
-        todo.done = !todo.done
-        rendertodosection()
-        })
+        
 
         let deletebuttonEl= document.createElement('button')
         deletebuttonEl.className='todo_delete'
         deletebuttonEl.innerText= 'Delete'
-        deletebuttonEl.addEventListener('click', function () {
-            state.todos.pop()
-            rendertodosection()
-          })
         
-
+        deletebuttonEl.addEventListener('click', function(){
+            deleteodo(todo.text)
+            render()
+        })
 
         todoitemEl.append(checkboxEl, todotextEl, deletebuttonEl)
 
@@ -110,21 +164,21 @@ function rendertodosection(){
         let appEl =document.querySelector('.app')
         appEl?.append(todosection)
 }
+
 function render(){
+
+    document.body.textContent = ''
+    
     let appEl= document.createElement('div')
     appEl.className= 'app'
     document.body.append(appEl)
 
-
-    document.body.textContent + ''
-
     renderapptodo()
-
+  
     renderoptionsection()
 
     rendertodosection()
 }
 render()
 
-//Mistake nr a million in javascript
-//Due to  family emergency I didn't have time to focus on the exercise 100%
+
